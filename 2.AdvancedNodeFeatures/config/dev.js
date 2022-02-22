ae85c6c15dcf5e9539c70e67152bd2e5d374e74a
grigoar <grigore.nath.j@gmail.com>
2022-02-22 21:11:21 +0200

    added blog-node-advance app

---
+++ 2.AdvancedNodeFeatures/config/dev.js
@@ -0,0 +1,7 @@
+//cognizant.udemy.com/course/advanced-node-for-developers/learn/lecture/9646804#questions/10049240
+https: module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
+};
