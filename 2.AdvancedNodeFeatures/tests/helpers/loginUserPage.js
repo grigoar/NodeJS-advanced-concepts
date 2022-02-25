fbce39c07e7bacb6737633653a7dd20775e6a617
grigoar <grigore.nath.j@gmail.com>
2022-02-25 20:41:39 +0200

    finished continous integration(CI) section

--- 2.AdvancedNodeFeatures/tests/helpers/loginUserPage.js
+++ 2.AdvancedNodeFeatures/tests/helpers/loginUserPage.js
@@ -8,6 +8,6 @@ Page.prototype.login = async function () {
 
   await this.setCookie({ name: 'session', value: session });
   await this.setCookie({ name: 'session.sig', value: sig });
-  await this.goto('localhost:3000');
+  await this.goto('http://localhost:3000');
   await this.waitFor('a[href="/auth/logout"]');
 };
